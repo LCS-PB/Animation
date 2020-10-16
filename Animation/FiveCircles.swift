b5c8a270b9a65588c5f7aa81ad1e19412d633a19
Patrick Boren <Pboren@lcs.on.ca>
2020-10-16 10:02:12 -0400

    circles finished

--- Animation/FiveCircles.swift
+++ Animation/FiveCircles.swift
@@ -15,6 +15,10 @@ class FiveCircles: NSObject, Sketchable {
     //       Therefore, the line immediately below must always be present.
     var canvas: Canvas
     var x = 250
+    var x2 = 250
+    var x3 = 250
+    var x4 = 250
+    var x5 = 250
     var y = 250
     // This function runs once
     override init() {
@@ -26,12 +30,26 @@ class FiveCircles: NSObject, Sketchable {
     
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
-        canvas.drawEllipse(at: Point(x: x + 25, y: 50), width: 50, height: 50)
-        x += 2
-
-        canvas.drawEllipse(at: Point(x: x + 25, y: 150), width: 50, height: 50)
+        canvas.drawShapesWithBorders = false
+        canvas.fillColor = Color.blue
+        canvas.drawEllipse(at: Point(x: x - 25, y: 125), width: 50, height: 50)
         x -= 1
+        canvas.fillColor = Color.green
+        canvas.drawEllipse(at: Point(x: x2 - 25, y: 225), width: 50, height: 50)
+        x2 += 1
+        canvas.fillColor = Color.orange
+        canvas.drawEllipse(at: Point(x: x3 - 25, y: 325), width: 50, height: 50)
+        x3 -= 1
+        canvas.fillColor = Color.purple
+        canvas.drawEllipse(at: Point(x: x4 - 25, y: 425), width: 50, height: 50)
+        x4 += 1
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(at: Point(x: x5 - 25, y: 25), width: 50, height: 50)
+        x5 += 1
+    
+    
     }
+    
     
 }
 
